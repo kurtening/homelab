b5d45c4ed59bf36e1d88744dbef359a5d2480bfc
kurtening <kurtranzelflores@gmail.com>
2026-07-27 16:40:45 +0800

    feat: protect the main branch

--- infrastructure/modules/github-repository/main.tf
+++ infrastructure/modules/github-repository/main.tf
@@ -17,3 +17,39 @@ resource "github_repository" "this" {
     prevent_destroy = true
   }
 }
+
+resource "github_repository_ruleset" "main" {
+  name        = "Protect main"
+  repository  = github_repository.this.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  rules {
+    deletion                = true
+    non_fast_forward        = true
+    required_linear_history = true
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = false
+      require_code_owner_review         = false
+      require_last_push_approval        = false
+      required_approving_review_count   = 0
+      required_review_thread_resolution = true
+    }
+
+    required_status_checks {
+      strict_required_status_checks_policy = true
+
+      required_check {
+        context = "OpenTofu and Terragrunt"
+      }
+    }
+  }
+}
